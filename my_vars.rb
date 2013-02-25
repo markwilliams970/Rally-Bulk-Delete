d7c2fe1643528051bccfa02f5d69ede372c4e4ea
Mark Williams <markw@rhat.mdwhome.local>
2013-02-24 21:42:35 -0700

    User-specified environment variables

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_base_url        = "https://rally1.rallydev.com/slm"
+$my_username        = "user@company.com"
+$my_password        = "password"
+$my_workspace       = "My Workspace"
+$my_project         = "My Project"
+$project_scope_down = true
+$wsapi_version      = "1.40"
