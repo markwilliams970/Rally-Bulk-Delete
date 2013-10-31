6e4f6726e553dd3cfb4ac4597fca71fe1ba39906
U-cirque\markwilliams <markwilliams@cirque.(none)>
2013-10-31 11:07:17 -0600

    User-specified environment variables

--- my_vars.rb
+++ my_vars.rb
@@ -1,7 +1,8 @@
 $my_base_url        = "https://rally1.rallydev.com/slm"
 $my_username        = "user@company.com"
-$my_password        = "password"
+$my_password        = "topsecret"
 $my_workspace       = "My Workspace"
 $my_project         = "My Project"
-$project_scope_down = true
-$wsapi_version      = "1.40"
+$project_scope_down = false
+$wsapi_version      = "1.43"
+$filename           = "items_to_delete.csv"
