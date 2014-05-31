b1e4d2be50640c44527a907ae0e629d280a84d62
markwilliams970 <mdwrally970@gmail.com>
2014-05-30 18:22:42 -0600

    User-specified environment variables

--- my_vars.rb
+++ my_vars.rb
@@ -6,3 +6,22 @@ $my_project         = "My Project"
 $project_scope_down = false
 $wsapi_version      = "1.43"
 $filename           = "items_to_delete.csv"
+
+# Valid Artifact Types. Note that the following assumes
+# Standard PI Types of Theme, Feature, and Initiative.
+# If user has custom PI Types, the following list will need to be
+# Customized to user environment
+$standard_types = ["US", "DE", "TA", "TC"]
+$pi_types = ["T", "I", "F"]
+
+$valid_types = [$standard_types, $pi_types].flatten
+
+$valid_query_types = {
+    "US" => :hierarchicalrequirement,
+    "DE" => :defect,
+    "TA" => :task,
+    "TC" => :testcase,
+    "F"  => "portfolioitem/feature",
+    "I"  => "portfolioitem/initiative",
+    "T"  => "portfolioitem/theme"
+}
